7af2a9e26919ec1c95784c8429125fc072cdfb86
jlorias <jlorias@hawaii.edu>
2020-12-07 00:17:30 -1000

    dummy map bc idk y map choropleth map not WORKING

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,119 +1,139 @@
-Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv', function(err, rows){
-  function unpack(rows, key) {
-    return rows.map(function(row) { return row[key]; });
-  }
 
-  var data = [{
-    type: 'Choroplethmapbox',
-    locations: unpack(rows, 'CODE'),
-    z: unpack(rows, 'GDP (BILLIONS)'),
-    text: unpack(rows, 'COUNTRY'),
-    colorscale: [
-      [0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],
-      [0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],
-      [0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']],
-    autocolorscale: false,
-    reversescale: true,
-    marker: {
-      line: {
-        color: 'rgb(180,180,180)',
-        width: 0.5
-      }
-    },
-    tick0: 0,
-    zmin: 0,
-    dtick: 1000,
-    colorbar: {
-      autotic: false,
-      tickprefix: '$',
-      title: 'GDP<br>Billions US$'
-    }
-  }];
+    Plotly.d3.csv('https://raw.githubusercontent.com/idemello/Cholera484/master/choleraDeathLocations.csv', function (err, rows) {
+      Plotly.d3.csv("https://raw.githubusercontent.com/idemello/Cholera484/master/choleraPumpLocations.csv", function (err2, rows2) {
+        function unpack(rows, key) {
+          return rows.map(function (row) {
+            return row[key];
+          });
+        }
 
-  var layout = {
-    dragmode: "zoom",
-    height: 2000,
-    width: 2500,
-    title: 'Countries by Average HDI Rank',
-    mapbox: {
-      style: 'light',
-      center: {
-        lat: 0,
-        lon: 0
-      },
-      domain: {
-        x: [0,1],
-        y: [0, 1]
-      },
-      zoom: 5
-    },
-    margin: {
-      r: 0,
-      t: 0,
-      b: 0,
-      l: 0,
-      pad: 0
-    }
-  };
-  Plotly.newPlot("world-map", data, layout, {showLink: false});
-});
+        var pumpLocLon = [];
+        var pumpLocLat = [];
 
-// Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv', function(err, rows){
-//   function unpack(rows, key) {
-//     return rows.map(function(row) { return row[key]; });
-//   }
-//
-//   var data = [{
-//     type: 'Choroplethmapbox',
-//     locations: unpack(rows, 'CODE'),
-//     z: unpack(rows, 'GDP (BILLIONS)'),
-//     text: unpack(rows, 'COUNTRY'),
-//     colorscale: [
-//       [0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],
-//       [0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],
-//       [0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']],
-//     autocolorscale: false,
-//     reversescale: true,
-//     marker: {
-//       line: {
-//         color: 'rgb(180,180,180)',
-//         width: 0.5
-//       }
-//     },
-//     tick0: 0,
-//     zmin: 0,
-//     dtick: 1000,
-//     colorbar: {
-//       autotic: false,
-//       tickprefix: '$',
-//       title: 'GDP<br>Billions US$'
-//     }
-//   }];
-//
-//   var layout = {
-//     dragmode: "zoom",
-//     height: 2000,
-//     width: 2500,
-//     title: 'Countries by Average HDI Rank',
-//     mapbox: {
-//       style: 'light',
-//       center: {
-//         lat: 0,
-//         lon: 0
-//       },
-//       domain: {
-//         x: [0,1],
-//         y: [0, 1]
-//       },
-//       zoom: 5
-//     },
-//     margin: {
-//       r: 0,
-//       t: 0,
-//       b: 0,
-//       l: 0,
-//       pad: 0
-//     }
-//   };
-//   Plotly.newPlot("map", data, layout);
-// });
+        //console.log(rows2);
+
+        pumpLocLon.push(Object.keys(rows2[0])[0]);
+        pumpLocLat.push(Object.keys(rows2[0])[1]);
+
+
+        for (var i = 0; i < rows2.length; i++) {
+          pumpLocLat.push(rows2[i]["51.513341"]);
+          pumpLocLon.push(rows2[i]["-0.136668"]);
+        }
+
+
+        /*for (var i = 0; i <= rows2.length; i++) {
+            rows.push({Deaths: "10.9999", PumpLon: pumpLocLon[i], PumpLat: pumpLocLat[i]});
+        }
+        */
+        //console.log(pumpLocLon);
+        //console.log(pumpLocLat);
+
+        console.log(pumpLocLat);
+        console.log(pumpLocLon);
+
+        // var newArr = Object.values(rows);
+        //console.log(newArr[0]);
+
+        for (i = 0; i < rows2.length; i++) {
+          // newArr[0].push('0');
+        }
+
+        //console.log(rows[0]['Deaths']);
+
+
+        var scl = [[0, "#a50026"], [0.1, "#d73027"], [0.2, "#f46d43"], [0.3, "#fdae61"], [0.4, "#fee090"], [0.5, "#ffffff"], [0.6, "#e0f3f8"], [0.7, "#313695"], [0.8, "#313695"], [0.9, "#3C2200"], [1, "#000"]];
+        var data = [{
+          type: 'scattermapbox',
+          mode: 'markers',
+          text: unpack(rows, 'Deaths'),
+          lon: unpack(rows, 'PumpLon'),
+          lat: unpack(rows, 'PumpLat'),
+          marker: {
+            color: 'rgb(255, 3, 3)',
+            autocolorscale: false,
+            cmin: 0,
+            cmax: 20,
+            reversescale: false,
+            opacity: 0.59,
+            size: unpack(rows, 'Deaths'),
+            sizeref: 0.3333333333,
+            colorbar: {
+              thickness: 10,
+              titleside: 'right',
+              outlinecolor: 'rgba(68,68,68,0)',
+              ticks: 'outside',
+              ticklen: 3,
+              shoticksuffix: 'last',
+              ticksuffix: ' deaths',
+              dtick: 1
+            }
+          },
+
+          name: 'Deaths'
+        }];
+
+        var pumpData = [{
+          type: 'scattermapbox',
+          mode: 'markers',
+          opacity: 0.5,
+          lon: pumpLocLon,
+          lat: pumpLocLat,
+          marker: {
+            symbol: 'square',
+            size: 15,
+            color: 'rgb(0,0,0)'
+          },
+          name: 'Pump'
+        }];
+
+        var layout = {
+
+          height: 1000,
+          width: 2000,
+          dragmode: 'zoom',
+          title: 'Deaths by Location From Cholera, London, August 19th, 1854 to September 29th 1854',
+          mapbox: {
+            center: {
+              lat: 51.51289,
+              lon: -0.136523
+            },
+            domain: {
+              x: [0, 1],
+              y: [0, 1]
+            },
+            style: 'dark',
+            zoom: 16.1
+          },
+          margin: {
+            r: 0,
+            t: 0,
+            b: 0,
+            l: 0,
+            pad: 0
+          },
+          showlegend: true,
+          legend: {
+            x: 0,
+            y: 1,
+            traceorder: 'normal',
+            font: {
+              family: 'sans-serif',
+              size: 12,
+              color: '#000'
+            },
+            sizeref: 0.3333,
+            bgcolor: '#E2E2E2',
+            bordercolor: '#FFFFFF',
+            borderwidth: 2
+          }
+        };
+
+        Plotly.setPlotConfig({
+          mapboxAccessToken: 'pk.eyJ1IjoiaWRlbWVsbG8iLCJhIjoiY2ptNnFiZmc5MDJoZzNwdXE2aXhiYXNqdCJ9.qKCVHMu9enwE6uzCJRDgFw'
+        });
+
+        Plotly.newPlot('myDiv', data, layout);
+        Plotly.plot('myDiv', pumpData, layout);
+      })
+    });
