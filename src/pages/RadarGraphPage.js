e3bf12e999eeed0bc4966c6cac1a9c6ebd868ee5
jiaayuxu <jx2@hawaii.edu>
2020-12-08 01:11:26 -1000

    test

--- src/pages/RadarGraphPage.js
+++ src/pages/RadarGraphPage.js
@@ -1,9 +1,19 @@
-data = [{
+data = [
+  {
   type: 'scatterpolar',
   r: [39, 28, 8, 7, 28, 39],
   theta: ['A','B','C', 'D', 'E', 'A'],
-  fill: 'toself'
-}]
+  fill: 'toself',
+  name: 'Group A'
+  },
+  {
+  type: 'scatterpolar',
+  r: [1.5, 10, 39, 31, 15, 1.5],
+  theta: ['A','B','C', 'D', 'E', 'A'],
+  fill: 'toself',
+  name: 'Group B'
+  }
+]
 
 layout = {
   polar: {
@@ -11,8 +21,7 @@ layout = {
       visible: true,
       range: [0, 50]
     }
-  },
-  showlegend: false
+  }
 }
 
 Plotly.newPlot("myDiv", data, layout)
